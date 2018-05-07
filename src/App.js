277ce67289d3cf6a3593afc90700c9d5d9439a10
Tom Ritmeier <t.ritmeier@dvz-mv.de>
2018-05-07 12:33:45 +0200

    add more data

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import {Button, Paper, TextField, Typography} from 'material-ui'
 import {MonthsLegend} from './components/monthLegend'
 import {Stash} from './components/stash'
 
-const paperStyle = {
-  margin: 20,
-  textAlign: 'center',
-  display: 'inline-block',
-};
-
-
 export default class App extends Component {
   state = {
     stash: [
       {monthYear: {month: 0, value: 100}, value: 12},
       {monthYear: {month: 1, value: 200}, value: 33},
-      {monthYear: {month: 2, value: 300}, value: -5}
+      {monthYear: {month: 2, value: 300}, value: -5},
+      {monthYear: {month: 3, value: 300}, value: -5},
+      {monthYear: {month: 4, value: 300}, value: -5},
+      {monthYear: {month: 5, value: 300}, value: -5},
+      {monthYear: {month: 6, value: 300}, value: -5},
+      {monthYear: {month: 7, value: 300}, value: -5},
+      {monthYear: {month: 8, value: 300}, value: -5},
+      {monthYear: {month: 9, value: 300}, value: -5},
+      {monthYear: {month: 10, value: 300}, value: -5},
+      {monthYear: {month: 11, value: 300}, value: -5},
     ],
     value: ''
   };
@@ -28,7 +30,7 @@ export default class App extends Component {
   handleButton = e => {
     e.preventDefault();
     if (this.state.value) {
-      this.setState(({stash, value}) => ({
+      this.setState(({stash}) => ({
         stash: [
           ...stash,
           {monthYear: {month: 4, value: 300}, value: 666}
@@ -43,7 +45,7 @@ export default class App extends Component {
     return (
         <div>
           <Paper>
-            <Typography variant='display1' align='center'gutterBottom>Hab ich noch Geld?</Typography>
+            <Typography variant='display1' align='center' gutterBottom>Hab ich noch Geld?</Typography>
             <form onSubmit={this.handleButton}>
               <TextField name='title' label='ignore me' value={title} onChange={this.handleChange} margin='normal' />
               <Button type='submit' color='primary' variant='raised'>
@@ -52,6 +54,7 @@ export default class App extends Component {
             </form>
           </Paper>
           <MonthsLegend/>
+          <Stash stash={stash}/>
           <Stash stash={stash}/>
         </div>
     )
