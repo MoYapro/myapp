3bede6ca2fc618ccfd16cb69ebc74ca612145073
Tom Ritmeier <t.ritmeier@dvz-mv.de>
2018-05-19 09:46:32 +0200

    create year month when doing add

--- src/components/add.js
+++ src/components/add.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Button, Paper, TextField} from 'material-ui'
+import {Button, Paper, TextField, MenuItem} from 'material-ui'
+import SelectField from 'material-ui/SelectField';
 
 const paperStyle = {
   margin: 20,
@@ -7,27 +8,42 @@ const paperStyle = {
   display: 'inline-block',
 };
 
+const months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
+
 export class Add extends React.Component {
 
-  state = {value: ''};
+  state = {
+    year: 2018,
+    month: 0,
+    value: ''
+  };
 
-  handleChange = (event) => {
+  handleAmountChange = (event) => {
     this.setState({value: event.target.value})
+  };
+
+  handleMonthChange = (event) => {
+    this.setState({month: event.target.month})
   };
 
   handleButton = e => {
     e.preventDefault();
-    let stuff = {value: this.state.value}
-    this.props.addMethod(stuff);
+    let stuff = {
+      'monthYear': {year: this.state.year, month: this.state.month},
+      'value': parseFloat(this.state.value)
+    };
 
+    console.log('Add stuff to stash: ', stuff);
+
+    this.props.addMethod(stuff);
   };
 
   render() {
     return (
         <Paper style={paperStyle}>
           <form onSubmit={this.handleButton}>
-            <TextField name='xxx' value={this.state.value}
-                       onChange={this.handleChange} margin='normal'/>
+            <TextField name='amount' value={this.state.value}
+                       onChange={this.handleAmountChange} margin='normal'/>
             <Button type='submit' color='primary' variant='raised'>
               Create
             </Button>
