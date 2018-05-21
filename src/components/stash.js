06be42e3785094af030ca73be7f296b6fee7c59a
Tom Ritmeier <t.ritmeier@dvz-mv.de>
2018-05-21 15:17:21 +0200

    refacoring in progress...

--- src/components/stash.js
+++ src/components/stash.js
@@ -46,23 +46,6 @@ export class Stash extends React.Component {
   };
 
   render() {
-    if (this.state.colapsed) {
-      return this.renderColapsed();
-    } else {
-      return this.renderExpanded();
-    }
-  }
-
-  static isSameMonthYear(item, monthYear) {
-    return item.monthYear.year === monthYear.year
-        && item.monthYear.month === monthYear.month;
-  }
-
-  static monthGrouping(item) {
-    return item.monthYear.year + '-' + item.monthYear.month;
-  }
-
-  renderColapsed() {
     let detailStyle = this.getDetailStyle();
     return (
         <div style={containerStyle}>
@@ -73,20 +56,29 @@ export class Stash extends React.Component {
                     let key = this.state.year + '-' + monthIndex;
                     let monthData = _(this.props.stash
                     .filter(item => Stash.isSameMonthYear(item, {year: this.state.year, month: monthIndex})))
-                    .groupBy(Stash.monthGrouping)
-                    .map((items, key) => ({
-                      'monthYear': key,
-                      'value': _.sumBy(items, 'value')
-                    })).value();
-                    if (monthData.length !== 1) {
+                    .groupBy(this.monthGrouping)
+                    .map(Stash.mapToSum).value();
+                    if (monthData.length === 0) {
                       return this.buildEmptyListItem(key);
                     }
-                    return (
-                        <ListItem key={monthData[0].monthYear} button onClick={this.handleMonthClick(monthData[0].monthYear)}
-                                  style={{backgroundColor: key === this.state.detailsForMonth ? 'lightskyblue' : ''}}>
-                          <ListItemText primary={monthData[0].value ? monthData[0].value : '0'}/>
-                        </ListItem>
-                    );
+                    else if (monthData.length === 1) {
+                      return (
+                          <ListItem key={key} button onClick={this.handleMonthClick(key)} style={{backgroundColor: key === this.state.detailsForMonth ? 'lightskyblue' : ''}}>
+                            <ListItemText primary={monthData[0].value ? monthData[0].value : '0'}/>
+                          </ListItem>
+                      );
+                    }
+                    else if (monthData.length === 2) {
+                      let positiveValue = 0;
+                      let negativeValue = 0;
+                      return (
+                          <ListItem key={key} button onClick={this.handleMonthClick(key)} style={{backgroundColor: key === this.state.detailsForMonth ? 'lightskyblue' : ''}}>
+                            <ListItemText primary={positiveValue} style={{width: 50}}/>
+                            <ListItemText primary={negativeValue} style={{width: 50}}/>
+                          </ListItem>
+                      );
+                    }
+                    return '';
                   }
               )}
             </List>
@@ -95,42 +87,17 @@ export class Stash extends React.Component {
             <MonthDetails monthYear={this.state.detailsForMonth} stash={this.props.stash} addMethod={this.props.addMethod} deleteMethod={this.props.deleteMethod}/>
           </Paper>
         </div>
-    )
+    );
   }
 
-  renderExpanded() {
-    let detailStyle = this.getDetailStyle();
-    let grouped = this.getExpandedData(this.props.stash);
-    console.log(grouped);
-    return (
-        <div style={containerStyle}>
-          <Paper style={paperStyle}>
-            <Button onClick={this.changeColapsed} color='primary' variant='raised'>
-              {this.state.colapsed ? '±' : '+|-'}
-            </Button>
-            <List>
-              {months.map((month, index) => {
-                    let key = this.state.year + '-' + index;
-                    let data = [];
-                    data[0] = grouped.filter(item => item.monthYear === key + '-positive')[0];
-                    data[1] = grouped.filter(item => item.monthYear === key + '-negative')[0];
-                    console.log('data', data);
-                    return (
-                        <ListItem key={key} button onClick={this.handleMonthClick(key)} style={{backgroundColor: key === this.state.detailsForMonth ? 'lightskyblue' : ''}}>
-                          <ListItemText primary={data[0] && data[0].value ? data[0].value : '0'} style={{width: 50}}/>
-                          <ListItemText primary={data[1] && data[1].value ? data[1].value : '0'}/>
-                        </ListItem>
-                    )
-                  }
-              )}
-            </List>
-          </Paper>
-          <Paper style={detailStyle}>
-            <MonthDetails monthYear={this.state.detailsForMonth} stash={this.props.stash} addMethod={this.props.addMethod}/>
-          </Paper>
-        </div>
-    )
+  static isSameMonthYear(item, monthYear) {
+    return item.monthYear.year === monthYear.year
+        && item.monthYear.month === monthYear.month;
   }
+
+  monthGrouping = item => item.monthYear.year + '-' + item.monthYear.month + (this.state.colapsed ? '' : (item.value >= 0 ? '-positive' : '-negative'));
+
+  static mapToSum = (items, key) => ({'monthYear': key, 'value': _.sumBy(items, 'value')});
 
   getDetailStyle() {
     return {
@@ -138,23 +105,5 @@ export class Stash extends React.Component {
       verticalAlign: 'top',
       marginTop: !this.state.detailsForMonth ? 20 : paperStyle.margin + monthHeight * this.state.detailsForMonth.split('-')[1]
     };
-  }
-
-  getGroupedData(stash) {
-    return _(stash)
-    .groupBy(stash => stash.monthYear.year + '-' + stash.monthYear.month)
-    .map((items, key) => ({
-      'monthYear': key,
-      'value': _.sumBy(items, 'value')
-    })).value();
-  }
-
-  getExpandedData(stash) {
-    return _(stash)
-    .groupBy(item => item.monthYear.year + '-' + item.monthYear.month + '-' + (item.value >= 0 ? 'positive' : 'negative'))
-    .map((items, key) => ({
-      'monthYear': key,
-      'value': _.sumBy(items, 'value')
-    })).value();
   }
 }
