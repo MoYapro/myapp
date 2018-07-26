dcf35d1430a0c49d613b467f760019939cc25e88
Tom Ritmeier <t.ritmeier@dvz-mv.de>
2018-07-26 21:24:30 +0200

    fix alignment of stashes

--- src/components/stash.js
+++ src/components/stash.js
@@ -10,6 +10,7 @@ import Typography from "material-ui/es/Typography/Typography";
 const containerStyle = {
   height: 406,
   display: 'inline-block',
+  verticalAlign: 'top'
 };
 
 export class Stash extends React.Component {
