6f7762ec0289b8df43829b1d5808216359d0ab74
Tom Ritmeier <t.ritmeier@dvz-mv.de>
2018-08-04 07:32:03 +0200

    fix addToStash test

--- src/businessLogic/state-functions.js
+++ src/businessLogic/state-functions.js
@@ -1,7 +1,7 @@
-import {stashOf} from 'data-functions';
+import {cloneOf} from './data-functions';
 
 export function addItem(stash, item) {
-  let newStash = stashOf(stash);
+  let newStash = cloneOf(stash);
   item.id = newStash.items.length + 1;
   newStash.items.push(item);
   return newStash;
